643d56651859fe2032d0fd028dc295404d4d60d5
Dustin Conrad <duconrad@paypal.com>
2015-01-07 13:30:22 -0500

    ghetto bracket going

--- resources/public/js/bracket.js
+++ resources/public/js/bracket.js
@@ -1,20 +1,35 @@
 function init(stageId) {
     var stage = new createjs.Stage(stageId);
-    addMatchup(stage, 100, 100);
+    initBracket(stage);
+}
+
+function drawBracket(stage, bracket) {
+    console.log(bracket);
+    addMatchup(stage, bracket.data.teams, 100, 100);
     stage.update();
 }
 
-function initBracketCallback(data) {
-
+function initBracket(stage) {
+    $.get("/api/bracket", function(data) {
+        drawBracket(stage, data);
+    });
 }
 
-function initBracket() {
-    $.get("/api/bracket", initBracketCallback());
-}
-
-function addMatchup(stage, x, y) {
+function addMatchup(stage, teams, x, y) {
+    var padding = 10;
+    var textSize = 50;
+    for (i = 0; i < teams.length; i++) {
+        var newY = y + padding + textSize * i;
+        var team = teams[i];
+        var text = new createjs.Text(team.name);
+        text.x = x + padding;
+        text.y = newY;
+        stage.addChild(text);
+    }
     var square = new createjs.Shape();
-    square.graphics.beginFill("DeepSkyBlue").drawRect(x, y, 100, 50);
+    square.graphics
+        .beginStroke("black")
+        .drawRect(x, y, 100, 2 * padding + teams.length * textSize);
     stage.addChild(square);
 }
 

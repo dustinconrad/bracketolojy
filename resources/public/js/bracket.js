0d0de1f0c04d95c87673848e95de1d375ceebf69
Dustin Conrad <duconrad@paypal.com>
2015-01-07 15:10:18 -0500

    before changing to something tableish

--- resources/public/js/bracket.js
+++ resources/public/js/bracket.js
@@ -18,18 +18,24 @@ function initBracket(stage) {
 function addMatchup(stage, teams, x, y) {
     var padding = 10;
     var textSize = 14;
+    var lineLength = 0;
     for (i = 0; i < teams.length; i++) {
         var newY = y + padding + textSize * i;
         var team = teams[i];
         var text = new createjs.Text(team.name, textSize + "px Arial");
         text.x = x + padding;
         text.y = newY;
+        if (text.getMeasuredWidth() > lineLength) {
+            lineLength = text.getMeasuredWidth();
+        }
         stage.addChild(text);
     }
     var square = new createjs.Shape();
+    var width = 2 * padding + lineLength;
+    var height = 2 * padding + teams.length * textSize;
     square.graphics
         .beginStroke("black")
-        .drawRect(x, y, 100, 2 * padding + teams.length * textSize);
+        .drawRect(x, y, width, height);
     stage.addChild(square);
 }
 

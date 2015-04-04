da7b096fc607969c716f48b0d99b7c010c792b1e
Dustin Conrad <dustinaconrad@gmail.com>
2015-04-04 15:21:18 -0400

    before refactor

--- resources/public/js/bracket.js
+++ resources/public/js/bracket.js
@@ -10,7 +10,6 @@ function initBracket(container) {
 
 function drawBracket(container, bracket) {
     makeRoot(container, bracket);
-
 }
 
 function makeRoot(container, bracket) {
@@ -25,42 +24,25 @@ function makeRoot(container, bracket) {
 
 function makeTable(teams) {
     var rows = [];
-    for (i = 0; i < teams.length; i++) {
+    for (var i = 0; i < Math.min(4, teams.length); i++) {
         var team = teams[i];
+        var name = team.name;
+        var avgPts = team["avg-pts"];
+        var expVal = team["expected-value"];
+        var prob = team["weight"];
+
         rows.push('<tr>');
-        rows.push('<td>' + team.name + '</td>');
-        rows.push('<td>' + team["avg-pts"].toFixed(2) + '</td>');
-        rows.push('<td>' + team["expected-value"].toFixed(2) + '</td>');
+        rows.push('<td>' + name + '</td>');
+        rows.push('<td>' + avgPts.toFixed(2) + '</td>');
+        rows.push('<td>' + expVal.toFixed(2) + '</td>');
+        rows.push('<td>' + prob.toFixed(2) + '</td>');
         rows.push('</tr>');
     }
     return '<table>' +
-                '<tr><th>Team</th><th>Avg. pts</th><th>Exp. value</th></tr>' +
+                '<tr><th>Team</th><th>AVG</th><th>EV</th><th>Prob.</th></tr>' +
                 rows.join("") +
            '</table>';
-
 }
 
-function addMatchup(stage, teams, x, y) {
-    var padding = 10;
-    var textSize = 14;
-    var lineLength = 0;
-    for (i = 0; i < teams.length; i++) {
-        var newY = y + padding + textSize * i;
-        var team = teams[i];
-        var text = new createjs.Text(team.name, textSize + "px Arial");
-        text.x = x + padding;
-        text.y = newY;
-        if (text.getMeasuredWidth() > lineLength) {
-            lineLength = text.getMeasuredWidth();
-        }
-        stage.addChild(text);
-    }
-    var square = new createjs.Shape();
-    var width = 2 * padding + lineLength;
-    var height = 2 * padding + teams.length * textSize;
-    square.graphics
-        .beginStroke("black")
-        .drawRect(x, y, width, height);
-    stage.addChild(square);
-}
+
 

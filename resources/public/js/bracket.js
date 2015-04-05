1c8d60f9ff6591b9136043badca983e8841adbd5
Dustin Conrad <dustinaconrad@gmail.com>
2015-04-05 13:32:50 -0400

    initial pass at a bracket in bootstrap grid

--- resources/public/js/bracket.js
+++ resources/public/js/bracket.js
@@ -1,30 +1,66 @@
+var COL_WIDTH = 2;
+var NUM_TEAMS = 4;
+
 function init(container) {
     initBracket(container);
 }
 
 function initBracket(container) {
     $.get("/api/bracket", function(data) {
-        drawBracket(container, data);
+        drawBracketHelper(container, data);
     });
 }
 
-function drawBracket(container, bracket) {
-    makeRoot(container, bracket);
+function treeDepth(bracket) {
+    return bracket["data"]["round"];
 }
 
-function makeRoot(container, bracket) {
-    var root = $(makeTable(bracket.data.teams));
-    root.addClass("table")
+function drawBracketHelper(container, bracket) {
+    var depth = treeDepth(bracket);
+    var rows = Math.pow(2, depth);
+
+    //roundHeaders
+    var roundHeaders = $('<div class="row" id="roundHeaders"></div>');
+    for (var r = 0; r < depth; r++) {
+        var roundHeader = $('<span>Round ' + (depth - r)  + '</span>')
+            .addClass('col-md-' + COL_WIDTH);
+        roundHeaders.append(roundHeader);
+    }
+    $(container).append(roundHeaders);
+
+    for (var i = rows - 1; i >= 0; i--) {
+        $(container).append('<div class="row" id="row-' + i + '"></div>');
+    }
+    drawBracket(container, bracket, 0, 0, rows)
+}
+
+function drawBracket(container, bracket, col, rowMin, rowMax) {
+    if (bracket && bracket["data"]["round"] > 0) {
+        var midRow = Math.round((rowMin + rowMax) / 2);
+        drawTable(container, bracket["data"]["teams"], col, midRow);
+        // upper
+        drawBracket(container, bracket["upper"], col + 1, midRow, rowMax);
+        // lower
+        drawBracket(container, bracket["lower"], col + 1, rowMin, midRow);
+    }
+}
+
+function drawTable(container, teams, col, row) {
+    var table = $(makeTable(teams));
+    table.addClass("table")
         .addClass("table-bordered")
-        .addClass("table-centered")
         .addClass("table-condensed")
         .addClass("small-text");
-    $(container).append(root);
+    var tableDiv = $('<div/>')
+        .addClass("col-md-offset-" + col*COL_WIDTH)
+        .addClass("col-md-" + COL_WIDTH)
+        .append(table);
+    $(container).find("#row-"+ row).append(tableDiv);
 }
 
 function makeTable(teams) {
     var rows = [];
-    for (var i = 0; i < Math.min(4, teams.length); i++) {
+    for (var i = 0; i < Math.min(NUM_TEAMS, teams.length); i++) {
         var team = teams[i];
         var name = team.name;
         var avgPts = team["avg-pts"];
@@ -39,10 +75,9 @@ function makeTable(teams) {
         rows.push('</tr>');
     }
     return '<table>' +
-                '<tr><th>Team</th><th>AVG</th><th>EV</th><th>Prob.</th></tr>' +
-                rows.join("") +
-           '</table>';
+        '<tr><th>Team</th><th>AVG</th><th>EV</th><th>Prob.</th></tr>' +
+        rows.join("") +
+        '</table>';
 }
-
 
 
